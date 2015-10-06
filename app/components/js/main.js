ca75231ddf553721bd89fddc1efc9cc400a65971
Brian Brennan <brianb@cardsandpockets.com>
2015-10-06 12:06:29 -0400

    fixed height / width issue

--- app/components/js/main.js
+++ app/components/js/main.js
@@ -18,12 +18,13 @@ fs.readdir(FILE_PATH, function(err, f){//gets files currently in directory
 		files = f;
 
 		for(var i = 0; i < files.length; i++){
-			s('.sidebar ul').insert("<li>" + files[i] + "</li>");
+			s('.sidebar ul').insert("<li value=\"" + i +"\">" + files[i] + "</li>");
 		}
 
 		s('.sidebar ul li').on('click', function(e){
 			s('.sidebar ul li').removeClass();
 			s(this).addClass('active');
+			loadFile(s(this).attr('value')[0]);
 		});
 	}
 
@@ -51,3 +52,23 @@ s('#minimize-window').on('click', function(e){
 	
 	wind.minimize();
 });
+
+//----------------------------------------------Functions
+
+function loadFile(n){
+	loading = true;
+	var num = new Number(n);
+
+	var loadedFile;
+
+	fs.readFile(FILE_PATH + files[num], function(err, data){
+		if(err)
+			loadedFile = err;
+		else
+			loadedFile = data.toString();
+
+		s('.main-view .empty').css('display','none');
+		s('.main-view .content pre').innerHtml(loadedFile);
+		loading = false;
+	});
+}
