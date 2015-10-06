790cab213616de58c2fd0094c2727140d12acdd8
Brian Brennan <brianb@cardsandpockets.com>
2015-10-06 12:54:35 -0400

    need to figure out file write

--- app/components/js/main.js
+++ app/components/js/main.js
@@ -1,10 +1,18 @@
 //------------------------------------------Configuration
 
-var remote = require('remote');
-var fs 				= require('fs');
+var remote 	= require('remote');
+var fs 		= require('fs');
+var Enigma 	= require('enigma.js');
+
+var e = new Enigma({
+	sequence: 'ascii',
+	rotorSettings: '99Taps',
+	spaces: true
+});
 
 var FILE_PATH		= './app/files/';
 var files = [];
+var active_file;
 
 var loading = false;
 var maxed = false;
@@ -53,6 +61,26 @@ s('#minimize-window').on('click', function(e){
 	wind.minimize();
 });
 
+s('#encrypt-file').on('click', function(e){
+
+	if(e)
+		return e;
+
+	fs.readFile('in.txt', function(err, data){
+	m = e.encrypt(data.toString());
+
+	fs.writeFile('encrypted.ej', m, function(err){
+
+		m = e.decrypt(m);
+
+		fs.writeFile('out.txt', m, function(err){
+
+		});
+
+	});
+});
+});
+
 //----------------------------------------------Functions
 
 function loadFile(n){
@@ -61,36 +89,6 @@ function loadFile(n){
 
 	var loadedFile;
 
-	var count = 0;
-		var load_id = setInterval(function(){
-			var ran = Math.random() * (126 - 33) + 33;
-			document.getElementById('e').innerHTML = String.fromCharCode(ran);
-
-			ran = Math.random() * (126 - 33) + 33;
-			document.getElementById('i').innerHTML = String.fromCharCode(ran);
-
-			ran = Math.random() * (126 - 33) + 33;
-			document.getElementById('m').innerHTML = String.fromCharCode(ran);
-
-			ran = Math.random() * (126 - 33) + 33;
-			document.getElementById('t').innerHTML = String.fromCharCode(ran);
-
-			if (count % 10 == 0) {
-				var ran = Math.random() * (126 - 33) + 33;
-				document.getElementById('n').innerHTML = String.fromCharCode(ran);
-
-				ran = Math.random() * (126 - 33) + 33;
-				document.getElementById('g').innerHTML = String.fromCharCode(ran);
-
-				ran = Math.random() * (126 - 33) + 33;
-				document.getElementById('a').innerHTML = String.fromCharCode(ran);
-
-				ran = Math.random() * (126 - 33) + 33;
-				document.getElementById('e2').innerHTML = String.fromCharCode(ran);
-			}
-
-			count++;
-		},100);
 
 	fs.readFile(FILE_PATH + files[num], function(err, data){
 		
@@ -99,16 +97,15 @@ function loadFile(n){
 		else
 			loadedFile = data.toString();
 
+		active_file = loadedFile;
+
 		s('.main-view .empty').css('display','none');
 		s('.main-view .content pre').innerHtml(loadedFile);
 
 
 		var pre_content = s('pre').innerHtml()[0];
 		s('.main-view .content pre').innerHtml(pre_content.replace(/^(.*)$/mg, "<span class=\"line\">$1</span>"));
-		
+
 		loading = false;
-		clearInterval(load_id);
-
-
 	});
 }
